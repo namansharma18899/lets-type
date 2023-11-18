db52ad2e9795b62b94420b9eebaab92bbbe4558f
Naman Sharma <namansharma18899@gmail.com>
2023-11-18 19:27:50 +0530

    add: can now change terminal size without breaking code

--- lets_type/app.py
+++ lets_type/app.py
@@ -1,19 +1,25 @@
 import curses
 import asyncio
 import time
-from utils import wrap_text, get_fresh_quote, get_logger
+from typing import Any
 
+from lets_type.utils import wrap_text, get_fresh_quote, get_logger
 
 logger = get_logger()
 
 
 def main(stdscr):
+    def handle_resize(stdscr):
+        height, width = stdscr.getmaxyx()
+        stdscr.clear()
+        stdscr.addstr(0, 0, f"Terminal resized to {height}x{width}")
+        stdscr.refresh()
+
     def play(stdscr):
         original_text = get_fresh_quote(2)
-        input_text = ""
         cursor_position = 0
         wrong = []
-        #<---------------------------------------->Setup Window var---------------------------------------->
+        # Setup Window
         curses.curs_set(1)  # Set the cursor to be visible
         stdscr.clear()
         curses.start_color()
@@ -22,19 +28,16 @@ def main(stdscr):
         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
         curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
         curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
-
-        window_height, window_width = stdscr.getmaxyx()
-        header_height = 1
-        footer_height = 1
+        input_text = ""
         header_text = "Lets Type"  # Header
         footer_text = "Press 'q' to exit & 'r' to replay"  # Footer
-        middle_height = window_height - header_height - footer_height
+        start_time = time.time()
+        header_height = 1
+        footer_height = 1
         middle_y = header_height + 1  # Middle portion with active cursor
         middle_x = 0
-        input_text = ""
-        start_time = time.time()
-
-        while True: # Event Loop
+        while True:  # Event Loop
+            window_height, window_width = stdscr.getmaxyx()
             stdscr.clear()
             stdscr.bkgd(curses.color_pair(3))
             stdscr.addstr(
@@ -77,44 +80,53 @@ def main(stdscr):
             try:
                 stdscr.move(middle_y, cursor_position)
             except Exception as e:
-                logger.info(f'window spec {window_height}, {window_width} \
-                            {middle_y}, {cursor_position}')
+                logger.info(
+                    f"window spec {window_height}, {window_width} \
+                            {middle_y}, {cursor_position}"
+                )
             stdscr.refresh()
-            if cursor_position >= len(original_text):
+            if len(input_text) >= len(original_text):
                 break
-            key = stdscr.getch()  # Get Current Character Input
-            input_text = input_text + chr(key)
+            KEY = stdscr.getch()  # Get Current Character Input
+            if KEY == curses.KEY_RESIZE:
+                window_height, window_width = stdscr.getmaxyx()
+                stdscr.clear()
+                logger.info(f'Terminal resized to {window_height}x {window_width}')
+                # stdscr.addstr(0, 0, f"Terminal resized to {height}x{width}")
+                stdscr.refresh()
+                # handle_resize(stdscr)
+                continue
+            input_text = input_text + chr(KEY)
             try:
-                if chr(key) != original_text[cursor_position]:
+                if chr(KEY) != original_text[cursor_position]:
                     wrong.append(cursor_position)
             except IndexError as ie:
                 break
             logger.debug(
-                f"Key -> {chr(key)} | cursorpis -> {cursor_position} | inpt-> {input_text} | org -> {original_text[cursor_position]}"
+                f"Key -> {chr(KEY)} | cursorpis -> {cursor_position} | \
+                inpt-> {input_text} | org -> {original_text[cursor_position]}"
             )
-            # if int((cursor_position+1)/window_width)>0:
-            #     cursor_position = int((cursor_position+1)%window_width)
-            #     middle_y+=1
-            # else:
-            #     cursor_position+=1
-            cursor_position+=1
-            if key == 10:  # Enter key to Exit
+            if int((cursor_position + 1) / window_width) > 0:
+                cursor_position = int((cursor_position + 1) % window_width)
+                middle_y += 1
+            else:
+                cursor_position += 1
+            if KEY == 10:  # Enter key to Exit
                 break
-        logger.info(f'wrong -> {wrong}, input -> {len(input_text)}')
+        logger.info(f"wrong -> {wrong}, input -> {len(input_text)}")
         elapsed_time = (time.time() - start_time) / 60
         logger.info(f"time diff -> {elapsed_time} text len {len(original_text)}")
         report_text = "Report"
         stdscr.addstr(
             middle_y + 12,
             len(report_text) + 1,
-            f"Speed: {int(len(input_text.split(' ')))/elapsed_time:.2f} WPM",
+            f"Speed: {int(len(input_text.split(' '))) / elapsed_time:.2f} WPM",
             curses.color_pair(1),
         )
         stdscr.addstr(
             middle_y + 13,
             len(report_text) + 1,
-            f"Accuracy: {int(((len(input_text) - len(wrong))* 100 )/len(input_text))}% WPM",
-            # f"Accuracy: {(len(set(original_text.split()).symmetric_difference(set(input_text.split())))//2 * 100)/ len(original_text.split()):.2f} WPM",
+            f"Accuracy: {int(((len(input_text) - len(wrong)) * 100) / len(input_text))}% WPM",
             curses.color_pair(1),
         )
         stdscr.addstr(
@@ -143,17 +155,77 @@ if __name__ == "__main__":
         print("Gracefully Exiting")
 
 
-class TypeRacer:
-    def __init__(self) -> None:
-        pass
+class TypingSession:
+    _start_time = None
+    _end_time = None
+    _accuracy = None
+    _speed = None
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    @start_time.setter
+    def start_time(self, value):
+        self._start_time = value
+
+    @property
+    def end_time(self):
+        return self._end_time
+
+    @end_time.setter
+    def end_time(self, value):
+        self._end_time = value
+
+    @property
+    def accuracy(self):
+        return self._accuracy
+
+    @accuracy.setter
+    def accuracy(self, value):
+        self._accuracy = value
 
 
 class LetsType:
     def __init__(self, stdscr) -> None:
-        pass
+        self.window = stdscr
+        self.wrong_inputs = []
+        self.cursor_position = 0
+        self.org_word_mtd = {
+            'cursor_position': [0, 0],  # x,y
+            'word': get_fresh_quote(2),
+        }
+        self.input_word_mtd = {
+            'cursor_position': [0, 10],  # x,y
+            'word': ''
+        }
+        self.session = {
+            'start_time': None,
+            'end_time': None,
+            'accuracy': None,
+            'speed': None,
+        }
+
+    def __setup_window(self):
+        curses.curs_set(1)  # Set the cursor to be visible
+        self.window.clear()
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
+        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        self.header_text = "Lets Type"  # Header
+        self.footer_text = "Press 'q' to exit & 'r' to replay"  # Footer
+        self.header_height = 1
+        self.footer_height = 1
+        self.banner_coord = [0, self.header_height + 1]
+        # self.banner+middle_y = self.header_height + 1  # Middle portion with active cursor
+        # self.middle_x = 0
 
     def setup(self):
-        # setup_window()
+        self.__setup_window()
+        self.start_time = time.time()
         # wait for text to proceed or used cached one incase InternetError
         # Play
         # Show stats
--- lets_type/utils.py
+++ lets_type/utils.py
@@ -7,10 +7,12 @@ def wrap_text(text):
 
 
 def get_logger():
-    Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+    log_format = "%(levelname)s %(asctime)s - %(message)s"
     logging.basicConfig(
-        filename="logfile.log", filemode="w", format=Log_Format, level=logging.INFO
-    )
+        filename="logfile.log",
+        filemode="w",
+        format=log_format,
+        level=logging.INFO)
     logger = logging.getLogger()
     return logger
 
